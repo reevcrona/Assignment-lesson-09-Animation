bb1c1d8d931021ccee0b006b3151cf2d7e460f49
Jreev <jacob.reevcrona@yahoo.com>
2024-09-19 19:41:06 +0200

    added clouds

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const moon = document.querySelector(".moon");
 
 
 sun.addEventListener("animationend", () => {
-   sun.classList.remove("move-animation")
-   moon.classList.add("move-animation")
+   sun.classList.remove("move-animation");
+   moon.classList.add("move-animation");
 })
 
 moon.addEventListener("animationend",() => {
-    moon.classList.remove("move-animation")
-   sun.classList.add("move-animation")
+    moon.classList.remove("move-animation");
+    sun.classList.add("move-animation");
+    
 })
