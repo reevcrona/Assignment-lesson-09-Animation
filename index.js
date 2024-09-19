e8de292cbe739e3c4c8b00e4c162d263f57ec81c
Jreev <jacob.reevcrona@yahoo.com>
2024-09-19 18:28:31 +0200

    added morning animation

---
+++ index.js
@@ -0,0 +1,16 @@
+const sun = document.querySelector(".sun");
+const moon = document.querySelector(".moon");
+
+
+
+
+
+sun.addEventListener("animationend", () => {
+   sun.classList.remove("move-animation")
+   moon.classList.add("move-animation")
+})
+
+moon.addEventListener("animationend",() => {
+    moon.classList.remove("move-animation")
+   sun.classList.add("move-animation")
+})
