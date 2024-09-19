605bf07934ca14ff8151c5b80656a31d9983f3a4
Jreev <jacob.reevcrona@yahoo.com>
2024-09-19 21:59:32 +0200

    finished clouds and stars

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sun = document.querySelector(".sun");
 const moon = document.querySelector(".moon");
-
+const stars = document.querySelectorAll(".star")
 
 
 
@@ -8,10 +8,21 @@ const moon = document.querySelector(".moon");
 sun.addEventListener("animationend", () => {
    sun.classList.remove("move-animation");
    moon.classList.add("move-animation");
+    
+   stars.forEach((star,index) => {
+    star.style.animationDelay = `${(index + 1) * 0.2}s`
+    star.classList.add("show")
+    
+   })
+  
 })
 
 moon.addEventListener("animationend",() => {
     moon.classList.remove("move-animation");
     sun.classList.add("move-animation");
+    
+    stars.forEach(star => {
+        star.classList.remove("show")
+       })
     
 })
